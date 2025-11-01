7459efe4b1df9daa1fd5823adba4a59afe48d0e9
Lindo XodeDreamer Mhlongo <53181440+xodedreamer@users.noreply.github.com>
2025-11-01 19:47:16 +0200

    Longest sentence implementation

--- TS.TechnicalTest/LongestSentanceAnswer.cs
+++ TS.TechnicalTest/LongestSentanceAnswer.cs
@@ -5,6 +5,30 @@ public class LongestSentanceAnswer
 {
     public static int Solution(string s)
     {
-        throw new NotImplementedException("Not completed yet");
+        int maxWordCount = 0;
+        //Charecters to expect at the end of the sentence.
+        char[] sentenceDelimeters = new char[] { '.', '?', '!' };
+
+        // Breaking the whole text into separate sentences.
+        // StringSplitOptions.RemoveEmptyEntries ensures I don't get empty strings.
+        string[] sentences = s.Split(sentenceDelimeters, StringSplitOptions.RemoveEmptyEntries);
+
+        // Checking each sentence, one by one, to count the words.
+        foreach (string sentence in sentences)
+        {
+            // Getting rid of any extra space at the beginning or end of the sentence
+            // and then splitting it up by single space.
+            string[] words = sentence.Trim().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            int currentWordCount = words.Length;
+
+            // Noting the count if the sentence is longer than previus sentence.
+            if (currentWordCount > maxWordCount)
+            {
+                maxWordCount = currentWordCount;
+            }
+        }
+
+        return maxWordCount;
     }
 }
