f0f2ffb4a2cead4011e4a28ab4e07329e95d5111
Lindo XodeDreamer Mhlongo <53181440+xodedreamer@users.noreply.github.com>
2025-11-01 19:58:55 +0200

    Deepest pit implementation: getting total points length

--- TS.TechnicalTest/DeepestPitAnswer.cs
+++ TS.TechnicalTest/DeepestPitAnswer.cs
@@ -4,6 +4,18 @@ public class DeepestPitAnswer
 {
     public static int Solution(int[] points)
     {
-        throw new NotImplementedException("Not completed yet");
+        //Getting the length of points.
+        int totalPoints = points.Length;
+
+        // Ensuring at least 3 points for a pit (P, Q, R).
+        if (totalPoints < 3)
+        {
+            return -1; 
+        }
+
+        int maxDepth = -1; // Using -1 as the initial 'no pit found' signal.
+        int P = 0; // Current Peak index
+
+        return maxDepth;
     }
 }
